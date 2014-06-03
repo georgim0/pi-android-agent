f2453bcf2663ab779442c9fd75e340bae999a15a
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-06-03 11:42:34 +0100

    Clean up

--- PIAndroidAgent/src/com/kupepia/piandroidagent/Widget.java
+++
@@ -1,9 +0,0 @@
-package com.kupepia.piandroidagent;
-
-import android.content.Context;
-import android.view.View;
-
-public abstract class Widget {
-	
-	public abstract View createView(Context c);
-}
