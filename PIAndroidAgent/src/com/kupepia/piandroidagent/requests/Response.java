f536d60efb00f89966b2e29a7897ca9247b27956
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-06-06 09:51:09 +0100

    Workaround for the JSONTokener error

--- PIAndroidAgent/src/com/kupepia/piandroidagent/requests/Response.java
+++ PIAndroidAgent/src/com/kupepia/piandroidagent/requests/Response.java
@@ -2,7 +2,6 @@ package com.kupepia.piandroidagent.requests;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONTokener;
 
 public class Response {
     private int code;
@@ -17,6 +16,7 @@ public class Response {
         return this.code;
     }
     
+    /*workaround, will accept only JSONArray*/
     public Object getBody() throws JSONException {
         return this.body.get(0);
     }
