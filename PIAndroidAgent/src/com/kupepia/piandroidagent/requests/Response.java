2d6457599fe4315da005b74b6202983e49ef13a3
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-06-06 09:48:43 +0100

    Encapsulates Response feature

--- PIAndroidAgent/src/com/kupepia/piandroidagent/requests/Response.java
+++ PIAndroidAgent/src/com/kupepia/piandroidagent/requests/Response.java
@@ -1,14 +1,14 @@
 package com.kupepia.piandroidagent.requests;
 
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class Response {
     private int code;
-    private JSONTokener body;
+    private JSONArray body;
     
-    protected Response(JSONTokener json, int code) {
+    protected Response(JSONArray json, int code) {
         this.code = code;
         this.body = json;
     }
@@ -18,6 +18,6 @@ public class Response {
     }
     
     public Object getBody() throws JSONException {
-        return this.body.nextValue();
+        return this.body.get(0);
     }
 }
