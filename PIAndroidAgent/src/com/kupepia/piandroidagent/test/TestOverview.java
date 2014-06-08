7626a931c3e7a0bd5b34d07fb0ad822febedb5a7
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-06-08 10:32:56 +0100

    Cleanup

--- PIAndroidAgent/src/com/kupepia/piandroidagent/test/TestOverview.java
+++ PIAndroidAgent/src/com/kupepia/piandroidagent/test/TestOverview.java
@@ -1,12 +1,5 @@
 package com.kupepia.piandroidagent.test;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-
-import org.json.JSONException;
-
 import com.kupepia.piandroidagent.features.Overview;
 import com.kupepia.piandroidagent.requests.CommunicationManager;
 
